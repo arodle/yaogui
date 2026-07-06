fe872a075bb201dc2493d06f21627af69baf03b1
unknown <lirenxuan@sensetime.com>
2026-07-06 09:49:41 +0800

    Point local dev API proxy to server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'http://152.136.139.29',
         changeOrigin: true
       }
     }
